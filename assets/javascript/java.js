a65ee679ba0911843d631710c406fd58dc7aaa30
Bri <bherreras23@gmail.com>
2018-05-26 14:33:21 -0700

    Portfolio w styling still in progress

---
+++ assets/javascript/java.js
@@ -0,0 +1,16 @@
+
+$('#portfolio-btn').click(function(){
+	$('.nav').removeClass('animated fadeOutLeftBig');
+	$('.nav').css('visibility', 'visible');
+	$('.nav').addClass('animated fadeInLeftBig');
+	$('#portfolio-btn').addClass('animated fadeOutUp');
+	$('#portfolio-back-btn').css('visibility', 'visible');
+	$('#portfolio-back-btn').removeClass('animated fadeOutUp');
+	$('#portfolio-back-btn').addClass('animated fadeInDown');
+});
+$('#portfolio-back-btn').click(function(){
+	$('.nav').addClass('animated fadeOutLeftBig');
+	$('#portfolio-back-btn').addClass('animated fadeOutUp');
+	$('#portfolio-btn').removeClass('animated fadeOutUp');
+	$('#portfolio-btn').addClass('animated fadeInDown');
+})
